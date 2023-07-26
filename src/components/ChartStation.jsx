4ce81719a8951912980be89aa6f25e821f675d28
RicardoCordovilla <ricardocordovillaf@gmail.com>
2023-07-26 17:17:20 -0500

    cambios textos y api para esp

---
+++ src/components/ChartStation.jsx
@@ -0,0 +1,241 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts'
+import { config } from '../config'
+import { io } from "socket.io-client";
+import { CSVLink } from "react-csv";
+import { ExportToExcel } from './ExportToExcel'
+import { BiHome } from 'react-icons/bi';
+import { PiMicrosoftExcelLogo } from 'react-icons/pi';
+
+
+const ChartStation = () => {
+    const { station } = useParams()
+    const navigate = useNavigate()
+
+    const digits = (num) => {
+        let digit = num < 10 ? '0' + num : num + ''
+        return digit
+    }
+
+    const formatDate = (date) => {
+        const fecha = new Date(date)
+        let stringDate = fecha.getFullYear() + '-' + digits(fecha.getMonth() + 1) + '-' + digits(fecha.getDate())
+        return stringDate
+    }
+    const formatDate1 = (date) => {
+        const fecha = new Date(date)
+        let stringDate = fecha.getFullYear() + '-' + digits(fecha.getMonth() + 1) + '-' + digits(fecha.getDate() + 2)
+        return stringDate
+    }
+
+    const formatTime = (date) => {
+        const time = new Date(date)
+        let stringTime = digits(time.getHours() + ':' + digits(time.getMinutes()))
+        return stringTime
+    }
+
+
+    const options = {
+        backgroundColor: '#848484',
+        color: "#212121"
+    }
+
+
+    const [registers, setRegisters] = useState()
+    const [allRegisters, setAllRegisters] = useState([])
+    const [csv, setCsv] = useState([])
+    const [data, setData] = useState()
+    const [update, setUpdate] = useState(0)
+    const [download, setDownload] = useState(false)
+    const [fetching, setFetching] = useState(false)
+    const [from, setFrom] = useState(formatDate(new Date()))
+    const [to, setTo] = useState(formatDate(new Date()))
+
+
+    const getRegistersRange = (from, toDate) => {
+        console.log(from)
+        console.log(formatDate1(toDate))
+        let to = formatDate1(toDate)
+        let url = config.db.baseurl + 'registers/' + station + '/date?'
+            + 'from=' + from
+            + '&to=' + to
+        console.log(url)
+        setFetching(true)
+        axios.get(url)
+            .then(response => {
+                console.log(response.data)
+                setRegisters(response.data)
+                setFetching(false)
+            })
+            .catch(err => console.log(err.data))
+    }
+
+    const getAllRegisters = () => {
+        let url = config.db.baseurl + 'registers/' + station
+        console.log(url)
+        setFetching(true)
+        axios.get(url)
+            .then(response => {
+                console.log(response.data)
+                setAllRegisters(response.data)
+                setFetching(false)
+            })
+            .catch(err => console.log(err.data))
+    }
+
+    const formatData = (data) => {
+        let dataformat = data?.map((reg, index) => ({ date: formatDate(reg.createdAt), time: formatTime(reg.createdAt), co: reg.values.CO, ox: reg.values.OX }))
+        return dataformat
+    }
+    const formatCsv = (data) => {
+        let csvformat = data?.map((reg, index) => ({ Nodo: station, fecha: formatDate(reg.createdAt), hora: formatTime(reg.createdAt), co: reg.values.CO, ox: reg.values.OX }))
+        return csvformat
+    }
+
+
+    useEffect(() => {
+
+
+        const interval = setInterval(() => {
+            setUpdate(update => update + 1)
+        }, 1000);
+        return () => clearInterval(interval);
+
+
+    }, [])
+
+    useEffect(() => {
+        console.log(update)
+        getRegistersRange(from, to)
+
+    }, [update])
+
+    useEffect(() => {
+        console.log(from)
+        console.log(to)
+        getRegistersRange(from, to)
+    }, [from, to])
+
+    useEffect(() => {
+        setData(formatData(registers))
+    }, [registers])
+
+    useEffect(() => {
+        setCsv(formatCsv(allRegisters))
+    }, [allRegisters])
+
+
+
+
+    return (
+        <div className=''>
+
+            <div className="navContainer">
+
+                <button
+                    className='navButton'
+                    onClick={() => navigate('/')}
+                    disabled={download && fetching}
+                > <BiHome />Inicio</button>
+
+                {/* <button
+                className='HomeButton'
+                onClick={() => getRegistersRange(from, to)}
+                disabled={download && fetching}
+            >Actualizar</button> */}
+
+                {!allRegisters.length > 0 &&
+                    <button
+                        className='navButton'
+                        onClick={() => getAllRegisters()}
+                        disabled={download && fetching}
+                    >Obtener todos los datos <PiMicrosoftExcelLogo /></button>
+                }
+                {allRegisters.length > 0 &&
+                    <ExportToExcel
+                        apiData={csv}
+                        fileName={`${station}`}
+                        station={station}
+                        setDownload={setDownload}
+                        fetching={fetching}
+                    />
+
+                    // <CSVLink
+                    //     className='downLoadBtn'
+                    //     data={csv}
+                    //     filename={`${station}.csv`}
+                    // // headers={headers}
+                    // >
+                    //     Descargar CSV
+                    // </CSVLink>
+                }
+
+            </div>
+
+
+            <div className="dateContainer">
+                <div className="dateField">
+                    <label htmlFor="">Desde:</label>
+                    <input type="date"
+                        value={from}
+                        onChange={(e) => setFrom(e.target.value)}
+                    />
+                </div>
+                <div className="dateField">
+                    <label htmlFor="">Hasta:</label>
+                    <input type="date"
+                        value={to}
+                        onChange={(e) => setTo(e.target.value)}
+                    />
+                </div>
+            </div>
+
+            <h1>{station}</h1>
+
+            <div className="chartsContainer">
+
+                <div className="chartContainer">
+                    <h3>Datos Co2</h3>
+                    <LineChart width={800} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }} >
+                        <Line type="monotone" dataKey="co"
+                            stroke={config.styles.linecolor}
+                            strokeWidth={config.styles.linewidth}
+                            animationDuration={500}
+                        />
+                        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                        <XAxis dataKey="time" />
+                        <YAxis dataKey={"co"} />
+                        <Tooltip animationDuration={200}
+                            itemStyle={options}
+                            contentStyle={options}
+                        />
+                    </LineChart>
+                </div>
+
+                <div className="chartContainer">
+                    <h3>Datos Oxígeno</h3>
+                    <LineChart width={800} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                        <Line type="monotone" dataKey="ox"
+                            stroke={config.styles.linecolor}
+                            strokeWidth={config.styles.linewidth}
+                            animationDuration={500}
+                        />
+                        <CartesianGrid stroke="#ccc" strokeDasharray="10 10" />
+                        <XAxis dataKey="time" />
+                        <YAxis dataKey={"ox"} />
+                        <Tooltip animationDuration={200}
+                            itemStyle={options}
+                            contentStyle={options}
+                        />
+                    </LineChart>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default ChartStation
---
+++ src/components/StationCard.jsx
@@ -0,0 +1,79 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { config } from '../config'
+import { useNavigate } from 'react-router'
+
+
+const StationCard = ({ info }) => {
+    console.log(info)
+    const navigate = useNavigate()
+
+    const [itemInfo, setItemInfo] = useState()
+    const [dbUpdated, setDbUpdated] = useState(0)
+
+    const digits = (num) => {
+        let digit = num < 10 ? '0' + num : num + ''
+        return digit
+    }
+
+    const formatDate = (date) => {
+        const fecha = new Date(date)
+        let stringDate = digits(fecha.getDate()) + '-' + digits(fecha.getMonth() + 1) + '-' + fecha.getFullYear()
+        return stringDate
+    }
+
+    const formatTime = (date) => {
+        const time = new Date(date)
+        let stringTime = digits(time.getHours() + ':' + digits(time.getMinutes()))
+        return stringTime
+    }
+
+
+    const getItemInfo = () => {
+        if (info.title) {
+            let url = config.db.baseurl + 'registers/' + info.title + '/last'
+            console.log(url)
+            axios.get(url)
+                .then(response => {
+                    console.log(response.data)
+                    setItemInfo(response.data)
+                })
+                .catch(err => console.log(err))
+        }
+    }
+
+
+    useEffect(() => {
+        getItemInfo()
+        const interval = setInterval(() => getItemInfo(), 1000);
+        return () => clearInterval(interval);
+    }, [])
+
+    return (
+        <div className='stationCard'>
+            <span
+                className='stationTitle'
+                onClick={() => itemInfo && navigate('/' + info?.title)}
+            >{info?.title}</span>
+
+            <div className="sensorInfoContainer">
+                <span className='infoLabel'>CO:</span>
+                <span className='infoText'>{itemInfo ? itemInfo?.values?.CO : 'sin datos'}</span>
+            </div>
+            <div className="sensorInfoContainer">
+                <span className='infoLabel'>OX:</span>
+                <span className='infoText'>{itemInfo ? itemInfo?.values?.OX : 'sin datos'}</span>
+            </div>
+            <div className="sensorInfoContainer">
+                <span className='infoLabel'>Fecha:</span>
+                <span className='infoText'>{itemInfo ? formatDate(itemInfo?.createdAt) : '-'}</span>
+            </div>
+            <div className="sensorInfoContainer">
+                <span className='infoLabel'>Hora:</span>
+                <span className='infoText'>{itemInfo ? formatTime(itemInfo?.createdAt) : '-'}</span>
+            </div>
+        </div>
+    )
+}
+
+export default StationCard
